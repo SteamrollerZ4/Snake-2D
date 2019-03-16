ce93429eae1340b9c0abaf8036d01fe809e36b95
Tendai D. Zhou <32673023+SteamrollerZ4@users.noreply.github.com>
2019-03-16 18:44:27 +0200

    Delete scoreBoard.h

--- scoreBoard.h
+++
@@ -1,7 +0,0 @@
-#include <SDL2/SDL.h>
-#include <mysql/mysql.h>
-
-SDL_bool updateBoard();
-SDL_bool checkScore(int score);
-int insertionSort(int score);
-SDL_bool uploadScore(int position, int score, char *name);
